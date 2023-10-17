28fe1a32952081cbebd9df6847595bb3b20af776
Yusuf1999-hub <80812815+Yusuf1999-hub@users.noreply.github.com>
2023-10-17 16:14:30 +0500

    Update dars.py
    sdfasdf

--- dars.py
+++ dars.py
@@ -6,3 +6,4 @@ print("Real madrid")
 =======
 print("ARSENAL IS FOREVER CHAMPION")
 >>>>>>> 8923da075226da0ecfd340f9610655c477ec6619
+sfslajdfljslfjlsakjdflksajdflasjl;dfjas;ljflasjdfljs;ldfjas;ljf;lasf
